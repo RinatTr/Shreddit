9cc1c9cdeae849292af58b157eff1d3e2e28177d
RinatTr <rinatreg@gmail.com>
2024-02-29 09:55:40 -0500

    vercel

--- backend/queries/q-index.js
+++ backend/queries/q-index.js
@@ -5,5 +5,5 @@ if (process.env.NODE_ENV !== 'production') {
   
 const pgp = require('pg-promise')({});
 const db = pgp(process.env.DATABASE_URL || "postgres://localhost/shreddit");
-
+console.log("Connected to DB", process.env.DATABASE_URL)
 module.exports = { db }
